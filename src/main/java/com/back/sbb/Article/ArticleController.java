f25d30718452ba29c5cb67eb45c4f62323d4c543
wooki0123 <wookimini22@gmail.com>
2026-05-04 16:39:25 +0900

    feat: 게시글 생성 폼 구현

--- src/main/java/com/back/sbb/Article/ArticleController.java
+++ src/main/java/com/back/sbb/Article/ArticleController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,5 +24,10 @@ public class ArticleController {
     @GetMapping("/article/create")
     public String create() {
         return "article_form";
+    }
+    @PostMapping("/article/create")
+    public String articleCreate(@RequestParam(value = "title") String title, @RequestParam(value = "content") String content) {
+        this.articleService.create(title, content);
+        return "redirect:/article/list";
     }
 }
--- src/main/java/com/back/sbb/Article/ArticleService.java
+++ src/main/java/com/back/sbb/Article/ArticleService.java
@@ -3,6 +3,7 @@ package com.back.sbb.Article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -12,5 +13,13 @@ public class ArticleService {
 
     public List<Article> getList() {
         return articleRepository.findAll();
+    }
+
+    public void create(String title, String content) {
+        Article article = new Article();
+        article.setTitle(title);
+        article.setContent(content);
+        article.setCreatedDate(LocalDateTime.now());
+        articleRepository.save(article);
     }
 }
